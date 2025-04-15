6239357a9fb186e4f78d13b257b29713c69d9147
Prakhar Sahu <prakhars762@gmail.com>
2025-04-16 00:25:13 +0530

    final commit

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,16 @@
+import Agent from '@/components/Agent'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <>
+      <h3>Interview Generation</h3>
+
+      <Agent userName="You" userId="user1" type="generate" />
+      </>
+    </div>
+  )
+}
+
+export default Page
